41e7a93fd5096c5dacebe2c616a793f626882ce3
umelanlioglu <umelanlioglu@gmail.com>
2025-05-28 23:17:28 +0300

    Update tartanair_CVPR_downloader.py

--- dataset/tartanair_CVPR_downloader.py
+++ dataset/tartanair_CVPR_downloader.py
@@ -1,1 +1,9 @@
+import gdown
+import zipfile
 
+def download_from_gdrive(file_id, output_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, output_path, quiet=False)
+
+    with zipfile.ZipFile(output_path, 'r') as zip_ref:
+        zip_ref.extractall("monocular_data")
