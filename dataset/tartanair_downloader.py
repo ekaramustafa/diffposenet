6e2b06a6b7e65b2f083bc189e7025c11eee66286
umelanlioglu <umelanlioglu@gmail.com>
2025-05-29 05:58:44 +0300

    Update tartanair_downloader.py

--- dataset/tartanair_downloader.py
+++ dataset/tartanair_downloader.py
@@ -63,7 +63,7 @@ class TartanAirDownloader(object):
         self._isloaded = True
 
     
-    def download(self, destination_path : str, environments : List[str] = ["hospital", "abandonedfactory", "soulcity"]):
+    def download(self, destination_path : str, environments : List[str] = ["hospital", "abandonedfactory"]):
         """
         Downloads files from the specified environment and saves them to the given local path.
 
@@ -127,5 +127,5 @@ class TartanAirDownloader(object):
 if __name__ == "__main__":
     downloader = TartanAirDownloader(dataset_info_path="/scratch/users/imelanlioglu21/comp447_project/diffposenet/dataset/datasets.txt")
     downloader.load(levellist=['Easy'], typelist=['image', 'flow'], cameralist=['left', 'flow'])
-    bl, lst = downloader.download("/scratch/users/imelanlioglu21/comp447_project/tartanair_dataset", ["hospital", "abandonedfactory", "soulcity"])
+    bl, lst = downloader.download("/scratch/users/imelanlioglu21/comp447_project/tartanair_dataset", ["hospital", "abandonedfactory"])
     print(lst)
