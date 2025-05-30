7ca7a8db162c72a7c99586d99ded5c69cc62c23a
umelanlioglu <umelanlioglu@gmail.com>
2025-05-31 02:19:00 +0300

    Update train.py

--- nflownet/train.py
+++ nflownet/train.py
@@ -1,8 +1,9 @@
-import sys
+mport sys
 import os
 import wandb
 import random
 import torch
+import time
 import numpy as np
 import torch.nn as nn
 from torchvision.utils import flow_to_image
@@ -61,26 +62,31 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
 
     if accelerator.is_local_main_process:
         print("\n============= Loading Datasets =============")
+    start = time.time()
     train_dataset = nflownet_dataloader(root_dir_path=train_root_dir)
+    if accelerator.is_local_main_process:
+        print(f"Train dataset loaded in {time.time() - start:.2f} seconds.")
     test_dataset = nflownet_dataloader(root_dir_path=test_root_dir)
+    if accelerator.is_local_main_process:
+        print(f"Test dataset loaded in {time.time() - start:.2f} seconds.")
     torch.cuda.empty_cache()
 
     # Take random 1/3 subset of test
-    # indices = torch.randperm(len(test_dataset)).tolist()[:len(test_dataset) // 3]
-    # test_dataset = Subset(test_dataset, indices)
+    # test_indices = torch.randperm(len(test_dataset)).tolist()[:len(test_dataset)]
+    # test_dataset = Subset(test_dataset, test_indices)
 
     # Take 2/100 subset of train
-    indices = torch.randperm(len(train_dataset)).tolist()[:len(test_dataset) // 50]
-    train_dataset = Subset(train_dataset, indices)
+    # indices = torch.randperm(len(train_dataset)).tolist()[:len(train_dataset)]
+    # train_dataset = Subset(train_dataset, indices)
 
     batch_size = batch_size * accelerator.num_processes
 
     
     if accelerator.is_local_main_process:
         print("\n============= Dataloaders =============")
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=20, pin_memory=False, persistent_workers=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=20, pin_memory=False, persistent_workers=False)
-    test_loader_log = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=False, persistent_workers=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=5, pin_memory=False, persistent_workers=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=5, pin_memory=False, persistent_workers=False)
+    test_loader_log = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=False, pin_memory=False, persistent_workers=False)
 
     if accelerator.is_local_main_process:
         print(f"Training set contains {len(train_dataset)} samples.")
@@ -107,7 +113,7 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
     # torch.autograd.set_detect_anomaly(True)
     for epoch in range(num_epochs):
         model.train()
-        running_train_loss = 0.0
+        running_train_loss = torch.tensor([0.0], device=accelerator.device)
         pbar = tqdm(train_loader, desc=f"Epoch [{epoch + 1}/{num_epochs}]", disable=not accelerator.is_local_main_process)
 
         for paired_batch, normal_flow_batch in pbar:
@@ -123,36 +129,38 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
             if accelerator.is_local_main_process:
                 wandb.log({"Loss": loss.item()})
 
-        avg_train_loss = running_train_loss / len(train_loader)
+        avg_train_loss = accelerator.gather(running_train_loss).sum()
+        avg_train_loss = avg_train_loss / len(train_loader)
         train_losses.append(avg_train_loss)
         print(f" Average Train Loss: {avg_train_loss:.6f}")
 
         # ------------------- Validation -------------------
         accelerator.wait_for_everyone()
-        print(f"🔄 Rank {accelerator.process_index} waiting before validation")
         
         model.eval()
         running_test_loss = 0.0
         pbar = tqdm(test_loader, desc=f"Epoch [{epoch + 1}/{num_epochs}]", disable=not accelerator.is_local_main_process)
         with torch.no_grad():
-            #for paired_batch, normal_flow_batch in pbar:
             for i, (paired_batch, normal_flow_batch) in enumerate(pbar):
                 outputs = model(paired_batch)
                 loss = criterion(outputs, normal_flow_batch)
+                
                 if torch.isnan(loss) or loss.item() > 1e5:
-                    #print(f"⚠️  Skipping batch in TRAIN: loss={loss.item()}")
                     print(f"⚠️ Batch {i} in test_loader caused loss={loss.item()}")
-                    continue 
-                running_test_loss += loss.item()
-                pbar.set_postfix({'Batch Loss': loss.item()})
+                    loss = torch.tensor([1e5], device=accelerator.device) 
+                all_losses = accelerator.gather(loss).sum()
+                
+                if accelerator.is_local_main_process:
+                    running_test_loss += all_losses.item()
+                pbar.set_postfix({'Batch Loss': all_losses.item()})
         
         avg_test_loss = running_test_loss / len(test_loader)
         test_losses.append(avg_test_loss)
         print(f"Epoch [{epoch + 1}/{num_epochs}], Validation Loss: {avg_test_loss:.6f}")
 
-
+        accelerator.wait_for_everyone()
         # ----- Log Sample Predictions to wandb -----
-        if accelerator.is_local_main_process:
+        if True: #accelerator.is_local_main_process:
             images_to_log = {}
             max_samples = 8
             with torch.no_grad():
@@ -173,20 +181,23 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
                         images_to_log[f"Sample {j+1} - Predicted Normal Flow"] = wandb.Image(flow_pred, caption="Predicted Normal Flow")
                         images_to_log[f"Sample {j+1} - Ground Truth Normal Flow"] = wandb.Image(flow_gt, caption="Ground Truth Normal Flow")
                     break
-                       
-            wandb.log({
-                "epoch": epoch + 1,
-                "train_loss": avg_train_loss,
-                "validation_loss": avg_test_loss,
-                **images_to_log
-            })
+                    
+            if accelerator.is_local_main_process:     
+                wandb.log({
+                    "epoch": epoch + 1,
+                    "train_loss": avg_train_loss,
+                    "validation_loss": avg_test_loss,
+                    **images_to_log
+                })
 
         accelerator.wait_for_everyone()
+
         if accelerator.is_local_main_process:
             if epoch % 20 == 0:
                 model_to_save = accelerator.unwrap_model(model)
                 torch.save(model_to_save.state_dict(), f"nflownet_epoch_{epoch}.pth")
                 print(f"Model saved to nflownet_epoch_{epoch}.pth")
+        accelerator.wait_for_everyone()
 
     # Save model after training
     accelerator.wait_for_everyone()
@@ -202,7 +213,7 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
 
 if __name__ == "__main__":
     num_epochs = 400
-    batch_size = 32
+    batch_size = 8
     train_root_dir = "/kuacc/users/imelanlioglu21/comp447_project/tartanair_dataset/train_data/"
     test_root_dir = "/kuacc/users/imelanlioglu21/comp447_project/tartanair_dataset/test_data/"
     train_losses, test_losses = train(num_epochs, batch_size, train_root_dir, test_root_dir)
