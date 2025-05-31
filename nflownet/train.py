0b14276b3b76cd03b53d254f5ab7a7db89d582f3
umelanlioglu <umelanlioglu@gmail.com>
2025-05-31 03:28:43 +0300

    Update train.py

--- nflownet/train.py
+++ nflownet/train.py
@@ -1,4 +1,4 @@
-mport sys
+import sys
 import os
 import wandb
 import random
@@ -15,7 +15,7 @@ from torch.utils.data import Subset
 
 from accelerate import Accelerator
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from dataset.nflownet_dataloader import nflownet_dataloader
+from dataset.nflownet_dataloader2 import nflownet_dataloader
 from nflownet.model import NFlowNet
 
 def set_seed(seed=42):
@@ -43,9 +43,7 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
             print(f"CUDA device count: {torch.cuda.device_count()}")
             for i in range(torch.cuda.device_count()):
                 print(f"  GPU {i}: {torch.cuda.get_device_name(i)}")
-
-        # print("Accelerator number of processes: ",  accelerator.num_processes)
-        # batch_size = batch_size * accelerator.num_processes    
+  
                 
         print("\n============= Setting Up Wandb =============")
         wandb.login(key="66820f29cb45c85261f7dfd317c43275e8d82562")
@@ -55,7 +53,7 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
             config={
                 "learning_rate": 0.001,
                 "epochs": num_epochs,
-                "batch_size": batch_size,
+                "batch_size": batch_size*4,
                 "optimizer": "Adam",
             }
         )
@@ -71,13 +69,13 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
         print(f"Test dataset loaded in {time.time() - start:.2f} seconds.")
     torch.cuda.empty_cache()
 
-    # Take random 1/3 subset of test
-    # test_indices = torch.randperm(len(test_dataset)).tolist()[:len(test_dataset)]
-    # test_dataset = Subset(test_dataset, test_indices)
-
     # Take 2/100 subset of train
     # indices = torch.randperm(len(train_dataset)).tolist()[:len(train_dataset)]
     # train_dataset = Subset(train_dataset, indices)
+    
+    # Take random 1/3 subset of test
+    # test_indices = torch.randperm(len(test_dataset)).tolist()[:len(test_dataset)]
+    # test_dataset = Subset(test_dataset, test_indices)
 
     batch_size = batch_size * accelerator.num_processes
 
@@ -129,11 +127,14 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
             if accelerator.is_local_main_process:
                 wandb.log({"Loss": loss.item()})
 
-        avg_train_loss = accelerator.gather(running_train_loss).sum()
-        avg_train_loss = avg_train_loss / len(train_loader)
+        #avg_train_loss = accelerator.gather(running_train_loss).sum()
+        #avg_train_loss = avg_train_loss / len(train_loader)
+        total_loss = accelerator.reduce(running_train_loss, reduction="sum")
+        avg_train_loss = total_loss.item() / accelerator.num_processes / len(train_loader)
         train_losses.append(avg_train_loss)
         print(f" Average Train Loss: {avg_train_loss:.6f}")
 
+        
         # ------------------- Validation -------------------
         accelerator.wait_for_everyone()
         
@@ -159,36 +160,38 @@ def train(num_epochs, batch_size, train_root_dir, test_root_dir):
         print(f"Epoch [{epoch + 1}/{num_epochs}], Validation Loss: {avg_test_loss:.6f}")
 
         accelerator.wait_for_everyone()
-        # ----- Log Sample Predictions to wandb -----
-        if True: #accelerator.is_local_main_process:
-            images_to_log = {}
-            max_samples = 8
-            with torch.no_grad():
-                for paired_batch, normal_flow_batch in test_loader_log:
-                    pred_flow = model(paired_batch)
-                    pred_flow = flow_to_image(pred_flow)
-                    gt_flow = flow_to_image(normal_flow_batch)
 
-                    batch_size = paired_batch.size(0)
-                    for j in range(min(batch_size, max_samples)):
-                        img1 = TF.to_pil_image(paired_batch[j][:3].cpu())
-                        img2 = TF.to_pil_image(paired_batch[j][3:].cpu())
-                        flow_pred = TF.to_pil_image(pred_flow[j].cpu())
-                        flow_gt = TF.to_pil_image(gt_flow[j].cpu())
-                        
-                        images_to_log[f"Sample {j+1} - Image 1"] = wandb.Image(img1, caption="Input Image 1")
-                        images_to_log[f"Sample {j+1} - Image 2"] = wandb.Image(img2, caption="Input Image 2")
-                        images_to_log[f"Sample {j+1} - Predicted Normal Flow"] = wandb.Image(flow_pred, caption="Predicted Normal Flow")
-                        images_to_log[f"Sample {j+1} - Ground Truth Normal Flow"] = wandb.Image(flow_gt, caption="Ground Truth Normal Flow")
-                    break
+        
+        
+        # ----- Log Sample Predictions to wandb -----
+        images_to_log = {}
+        max_samples = 8
+        with torch.no_grad():
+            for paired_batch, normal_flow_batch in test_loader_log:
+                pred_flow = model(paired_batch)
+                pred_flow = flow_to_image(pred_flow)
+                gt_flow = flow_to_image(normal_flow_batch)
+
+                batch_size = paired_batch.size(0)
+                for j in range(min(batch_size, max_samples)):
+                    img1 = TF.to_pil_image(paired_batch[j][:3].cpu())
+                    img2 = TF.to_pil_image(paired_batch[j][3:].cpu())
+                    flow_pred = TF.to_pil_image(pred_flow[j].cpu())
+                    flow_gt = TF.to_pil_image(gt_flow[j].cpu())
                     
-            if accelerator.is_local_main_process:     
-                wandb.log({
-                    "epoch": epoch + 1,
-                    "train_loss": avg_train_loss,
-                    "validation_loss": avg_test_loss,
-                    **images_to_log
-                })
+                    images_to_log[f"Sample {j+1} - Image 1"] = wandb.Image(img1, caption="Input Image 1")
+                    images_to_log[f"Sample {j+1} - Image 2"] = wandb.Image(img2, caption="Input Image 2")
+                    images_to_log[f"Sample {j+1} - Predicted Normal Flow"] = wandb.Image(flow_pred, caption="Predicted Normal Flow")
+                    images_to_log[f"Sample {j+1} - Ground Truth Normal Flow"] = wandb.Image(flow_gt, caption="Ground Truth Normal Flow")
+                break
+                
+        if accelerator.is_local_main_process:     
+            wandb.log({
+                "epoch": epoch + 1,
+                "train_loss": avg_train_loss,
+                "validation_loss": avg_test_loss,
+                **images_to_log
+            })
 
         accelerator.wait_for_everyone()
 
